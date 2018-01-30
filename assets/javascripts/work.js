81dcf9571e0feeffc46b396686d9b557a72324ef
shaily <shaily@github.com>
2018-01-30 11:03:37 +0530

    changes in prototyping

--- assets/javascripts/work.js
+++ assets/javascripts/work.js
@@ -7,34 +7,45 @@ WB.Work.prototype= {
   initialize:function() {
     this.mouseOver();
     this.mouseOut();
-},
-mouseOver:function () {
-  document.getElementById("demo").onmouseover = function(){
-  document.getElementById("demo").style.color = "red";
- }
-},
-
-mouseOut:function ()  {
-  document.getElementById("demo").onmouseout = function(){
-  document.getElementById("demo").style.color = "black";
-}
-}
-}
-
-function abc(){
-  document.getElementById('id01').style.display='none';
-  var modal = document.getElementById('id01');
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
+    this.joinb();
+    this.myForm2();
+  },
+  
+  mouseOver:function () {
+    document.getElementById("demo").onmouseover = function(){
+      document.getElementById("demo").style.color = "red";
     }
-  }
-}
-function myForm2() {
-  var form2 = document.getElementById('form2');
-  form2.onsubmit=function(e) {
-  e.preventDefault();
-  form2.innerHTML= 'Thankyou, ' + form2.uname.value + '.  For you interest !';
-  form2.reset();
+  },
+
+  mouseOut:function ()  {
+    document.getElementById("demo").onmouseout = function(){
+      document.getElementById("demo").style.color = "black";
+    }
+  },
+
+  joinb:function (){
+    $('#join1').click(function(){
+      document.getElementById('id01').style.display='block';
+    });
+    $('#join').click(function(){
+      document.getElementById('id01').style.display='none';
+      var modal = document.getElementById('id01');
+      window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      }
+    });
+  },
+
+  myForm2:function () {
+    $('#subm').click(function(){
+      var form2 = document.getElementById('form2');
+      form2.onsubmit=function(e) {
+      e.preventDefault();
+      form2.innerHTML= 'Thankyou, ' + form2.uname.value + '.  For you interest !';
+      form2.reset();
+      }
+    });
   }
 }
